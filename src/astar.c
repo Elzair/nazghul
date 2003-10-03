fe7036d9e3c50e1c9184d872bbaa3039811967c5
Gordon McNutt <gmcnutt@users.sourceforge.net>
2003-10-03 04:14:16 +0000

    Pathfinding now aborts immediately if the target destination is invalid. This helps when you're in a ship in a wilderness with hostile npc's and your machine is slow.

--- src/astar.c
+++ src/astar.c
@@ -299,6 +299,15 @@ struct astar_node *astar_search(struct astar_search_info *info)
         int cost = 0;
         int goodness = 0;
 
+
+        // ---------------------------------------------------------------------
+        // We can save ourselves a long, fruitless search by checking if the
+        // final destination is valid and bailing out now if it isn't.
+        // ---------------------------------------------------------------------
+
+        if (! info->is_valid_location(info->context, info->x1, info->y1))
+                return NULL;
+
 	astar_search_init();
 
         info->heuristic(info, &goodness, &cost);
