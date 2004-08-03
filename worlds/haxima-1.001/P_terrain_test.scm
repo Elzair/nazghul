4509b4feae8a4d81e74638d2fac8e3f6f277f342
Sam Glasby <sglasby@users.sourceforge.net>
2004-08-03 03:56:33 +0000

    Tidied up the Sprite Gallery (chamber 1, missiles)

--- worlds/haxima-1.001/P_terrain_test.scm
+++ worlds/haxima-1.001/P_terrain_test.scm
@@ -7,7 +7,7 @@
   ;                               1  1  1  1  1  1  1  1  1
   ; 0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6  7  8
  (list
-  "xx .S .P .R .I .T .E @  @  .G .A .L .L .E .R .Y @  .I xx "
+  "xx [ .S .P .R .I .T .E  @  @  .G .A .L .L .E .R .Y ]  xx "
   "xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx "
   "xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx "
   "xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx "
@@ -23,7 +23,7 @@
   "xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx "
   "xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx "
   "xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx "
-  "xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx "
+  "xx .. .. .M .I .S .S .I .L .E .S .. .. .. .. .. .. .. xx "
   "xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx "
   "xx xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx xx "
   "xx .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. xx "
@@ -63,84 +63,84 @@
   (mk-obj-type tag name sprite layer-tfeat nil))
 
 ;; Lightning bolts:
-(mk-scenery-type 't_lightning_bolt_red        "a lightning bolt" s_lightning_bolt_red)
-(mk-scenery-type 't_lightning_bolt_green      "a lightning bolt" s_lightning_bolt_green)
-(mk-scenery-type 't_lightning_bolt_blue       "a lightning bolt" s_lightning_bolt_blue)
-(mk-scenery-type 't_lightning_bolt_cyan       "a lightning bolt" s_lightning_bolt_cyan)
-(mk-scenery-type 't_lightning_bolt_purple     "a lightning bolt" s_lightning_bolt_purple)
-(mk-scenery-type 't_lightning_bolt_yellow     "a lightning bolt" s_lightning_bolt_yellow)
-(mk-scenery-type 't_lightning_bolt_brown      "a lightning bolt" s_lightning_bolt_brown)
-(mk-scenery-type 't_lightning_bolt_orange     "a lightning bolt" s_lightning_bolt_orange)
-(mk-scenery-type 't_lightning_bolt_white      "a lightning bolt" s_lightning_bolt_white)
-(mk-scenery-type 't_lightning_bolt_grey       "a lightning bolt" s_lightning_bolt_grey)
-(mk-scenery-type 't_lightning_bolt_dark_grey  "a lightning bolt" s_lightning_bolt_dark_grey)
-(mk-scenery-type 't_lightning_bolt_chromatic  "a lightning bolt" s_lightning_bolt_chromatic)
+(mk-scenery-type 't_lightning_bolt_red        "lightning bolt" s_lightning_bolt_red)
+(mk-scenery-type 't_lightning_bolt_green      "lightning bolt" s_lightning_bolt_green)
+(mk-scenery-type 't_lightning_bolt_blue       "lightning bolt" s_lightning_bolt_blue)
+(mk-scenery-type 't_lightning_bolt_cyan       "lightning bolt" s_lightning_bolt_cyan)
+(mk-scenery-type 't_lightning_bolt_purple     "lightning bolt" s_lightning_bolt_purple)
+(mk-scenery-type 't_lightning_bolt_yellow     "lightning bolt" s_lightning_bolt_yellow)
+(mk-scenery-type 't_lightning_bolt_brown      "lightning bolt" s_lightning_bolt_brown)
+(mk-scenery-type 't_lightning_bolt_orange     "lightning bolt" s_lightning_bolt_orange)
+(mk-scenery-type 't_lightning_bolt_white      "lightning bolt" s_lightning_bolt_white)
+(mk-scenery-type 't_lightning_bolt_grey       "lightning bolt" s_lightning_bolt_grey)
+(mk-scenery-type 't_lightning_bolt_dark_grey  "lightning bolt" s_lightning_bolt_dark_grey)
+(mk-scenery-type 't_lightning_bolt_chromatic  "lightning bolt" s_lightning_bolt_chromatic)
 ;; Magic ball (fireball core or similar):
-(mk-scenery-type 't_fireball_red        "a fireball" s_magic_ball_core_red)
-(mk-scenery-type 't_fireball_green      "a fireball" s_magic_ball_core_green)
-(mk-scenery-type 't_fireball_blue       "a fireball" s_magic_ball_core_blue)
-(mk-scenery-type 't_fireball_cyan       "a fireball" s_magic_ball_core_cyan)
-(mk-scenery-type 't_fireball_purple     "a fireball" s_magic_ball_core_purple)
-(mk-scenery-type 't_fireball_yellow     "a fireball" s_magic_ball_core_yellow)
-(mk-scenery-type 't_fireball_brown      "a fireball" s_magic_ball_core_brown)
-(mk-scenery-type 't_fireball_orange     "a fireball" s_magic_ball_core_orange)
-(mk-scenery-type 't_fireball_white      "a fireball" s_magic_ball_core_white)
-(mk-scenery-type 't_fireball_grey       "a fireball" s_magic_ball_core_grey)
-(mk-scenery-type 't_fireball_dark_grey  "a fireball" s_magic_ball_core_dark_grey)
-(mk-scenery-type 't_fireball_chromatic  "a fireball" s_magic_ball_core_chromatic)
+(mk-scenery-type 't_fireball_red        "fireball" s_magic_ball_core_red)
+(mk-scenery-type 't_fireball_green      "fireball" s_magic_ball_core_green)
+(mk-scenery-type 't_fireball_blue       "fireball" s_magic_ball_core_blue)
+(mk-scenery-type 't_fireball_cyan       "fireball" s_magic_ball_core_cyan)
+(mk-scenery-type 't_fireball_purple     "fireball" s_magic_ball_core_purple)
+(mk-scenery-type 't_fireball_yellow     "fireball" s_magic_ball_core_yellow)
+(mk-scenery-type 't_fireball_brown      "fireball" s_magic_ball_core_brown)
+(mk-scenery-type 't_fireball_orange     "fireball" s_magic_ball_core_orange)
+(mk-scenery-type 't_fireball_white      "fireball" s_magic_ball_core_white)
+(mk-scenery-type 't_fireball_grey       "fireball" s_magic_ball_core_grey)
+(mk-scenery-type 't_fireball_dark_grey  "fireball" s_magic_ball_core_dark_grey)
+(mk-scenery-type 't_fireball_chromatic  "fireball" s_magic_ball_core_chromatic)
 ;; Magic burst/bloom/aura/field (5 ball lightnings in one tile):
-(mk-scenery-type 't_magic_field_red           "a magic field" s_magic_field_red)
-(mk-scenery-type 't_magic_field_green         "a magic field" s_magic_field_green)
-(mk-scenery-type 't_magic_field_blue          "a magic field" s_magic_field_blue)
-(mk-scenery-type 't_magic_field_cyan          "a magic field" s_magic_field_cyan)
-(mk-scenery-type 't_magic_field_purple        "a magic field" s_magic_field_purple)
-(mk-scenery-type 't_magic_field_yellow        "a magic field" s_magic_field_yellow)
-(mk-scenery-type 't_magic_field_brown         "a magic field" s_magic_field_brown)
-(mk-scenery-type 't_magic_field_orange        "a magic field" s_magic_field_orange)
-(mk-scenery-type 't_magic_field_white         "a magic field" s_magic_field_white)
-(mk-scenery-type 't_magic_field_grey          "a magic field" s_magic_field_grey)
-(mk-scenery-type 't_magic_field_dark_grey     "a magic field" s_magic_field_dark_grey)
-(mk-scenery-type 't_magic_field_blue_2        "a magic field" s_magic_field_blue_2)
-(mk-scenery-type 't_magic_field_red_pulsed    "a pulsing magic field" s_magic_field_red_pulsed)
-(mk-scenery-type 't_magic_field_green_pulsed  "a pulsing magic field" s_magic_field_green_pulsed)
-(mk-scenery-type 't_magic_field_blue_pulsed   "a pulsing magic field" s_magic_field_blue_pulsed)
-(mk-scenery-type 't_magic_field_white_pulsed  "a pulsing magic field" s_magic_field_white_pulsed)
-(mk-scenery-type 't_magic_field_purple_pulsed "a pulsing magic field" s_magic_field_purple_pulsed)
-(mk-scenery-type 't_magic_field_brown_pulsed  "a pulsing magic field" s_magic_field_brown_pulsed)
-(mk-scenery-type 't_magic_field_blue_2_pulsed "a pulsing magic field" s_magic_field_blue_2_pulsed)
+(mk-scenery-type 't_magic_field_red           "magic field" s_magic_field_red)
+(mk-scenery-type 't_magic_field_green         "magic field" s_magic_field_green)
+(mk-scenery-type 't_magic_field_blue          "magic field" s_magic_field_blue)
+(mk-scenery-type 't_magic_field_cyan          "magic field" s_magic_field_cyan)
+(mk-scenery-type 't_magic_field_purple        "magic field" s_magic_field_purple)
+(mk-scenery-type 't_magic_field_yellow        "magic field" s_magic_field_yellow)
+(mk-scenery-type 't_magic_field_brown         "magic field" s_magic_field_brown)
+(mk-scenery-type 't_magic_field_orange        "magic field" s_magic_field_orange)
+(mk-scenery-type 't_magic_field_white         "magic field" s_magic_field_white)
+(mk-scenery-type 't_magic_field_grey          "magic field" s_magic_field_grey)
+(mk-scenery-type 't_magic_field_dark_grey     "magic field" s_magic_field_dark_grey)
+(mk-scenery-type 't_magic_field_blue_2        "magic field" s_magic_field_blue_2)
+(mk-scenery-type 't_magic_field_red_pulsed    "pulsing magic field" s_magic_field_red_pulsed)
+(mk-scenery-type 't_magic_field_green_pulsed  "pulsing magic field" s_magic_field_green_pulsed)
+(mk-scenery-type 't_magic_field_blue_pulsed   "pulsing magic field" s_magic_field_blue_pulsed)
+(mk-scenery-type 't_magic_field_white_pulsed  "pulsing magic field" s_magic_field_white_pulsed)
+(mk-scenery-type 't_magic_field_purple_pulsed "pulsing magic field" s_magic_field_purple_pulsed)
+(mk-scenery-type 't_magic_field_brown_pulsed  "pulsing magic field" s_magic_field_brown_pulsed)
+(mk-scenery-type 't_magic_field_blue_2_pulsed "pulsing magic field" s_magic_field_blue_2_pulsed)
 ;; Chromatic ball lightning (8 frames):
-(mk-scenery-type 't_magic_field_chromatic_pulsed "a pulsing magic field" s_magic_field_chromatic_pulsed)
+(mk-scenery-type 't_magic_field_chromatic_pulsed "pulsing magic field" s_magic_field_chromatic_pulsed)
 ;; Lightning discharges:
 (mk-scenery-type 't_lightning_discharge_red   "lightning" s_lightning_discharge_red)
 (mk-scenery-type 't_lightning_discharge_green "lightning" s_lightning_discharge_green)
 (mk-scenery-type 't_lightning_discharge_blue  "lightning" s_lightning_discharge_blue)
 (mk-scenery-type 't_lightning_discharge_black "lightning" s_lightning_discharge_black)
 ;; Lightning discharges in cloud:
-(mk-scenery-type 't_lightning_cloud_red    "a lightning cloud" s_lightning_cloud_blue_red)
-(mk-scenery-type 't_lightning_cloud_green  "a lightning cloud" s_lightning_cloud_blue_green)
-(mk-scenery-type 't_lightning_cloud_blue   "a lightning cloud" s_lightning_cloud_blue_blue)
-(mk-scenery-type 't_lightning_cloud_purple "a lightning cloud" s_lightning_cloud_blue_purple)
-(mk-scenery-type 't_lightning_cloud_yellow "a lightning cloud" s_lightning_cloud_blue_yellow)
-(mk-scenery-type 't_lightning_cloud_orange "a lightning cloud" s_lightning_cloud_blue_orange)
+(mk-scenery-type 't_lightning_cloud_red    "lightning cloud" s_lightning_cloud_blue_red)
+(mk-scenery-type 't_lightning_cloud_green  "lightning cloud" s_lightning_cloud_blue_green)
+(mk-scenery-type 't_lightning_cloud_blue   "lightning cloud" s_lightning_cloud_blue_blue)
+(mk-scenery-type 't_lightning_cloud_purple "lightning cloud" s_lightning_cloud_blue_purple)
+(mk-scenery-type 't_lightning_cloud_yellow "lightning cloud" s_lightning_cloud_blue_yellow)
+(mk-scenery-type 't_lightning_cloud_orange "lightning cloud" s_lightning_cloud_blue_orange)
 ;; Magical spheres:
-(mk-scenery-type 't_magic_sphere_red    "a magical sphere" s_magic_sphere_red)
-(mk-scenery-type 't_magic_sphere_green  "a magical sphere" s_magic_sphere_green)
-(mk-scenery-type 't_magic_sphere_blue   "a magical sphere" s_magic_sphere_blue)
-(mk-scenery-type 't_magic_sphere_yellow "a magical sphere" s_magic_sphere_yellow)
-(mk-scenery-type 't_magic_sphere_purple "a magical sphere" s_magic_sphere_purple)
-(mk-scenery-type 't_magic_sphere_cyan   "a magical sphere" s_magic_sphere_cyan)
+(mk-scenery-type 't_magic_sphere_red    "magical sphere" s_magic_sphere_red)
+(mk-scenery-type 't_magic_sphere_green  "magical sphere" s_magic_sphere_green)
+(mk-scenery-type 't_magic_sphere_blue   "magical sphere" s_magic_sphere_blue)
+(mk-scenery-type 't_magic_sphere_yellow "magical sphere" s_magic_sphere_yellow)
+(mk-scenery-type 't_magic_sphere_purple "magical sphere" s_magic_sphere_purple)
+(mk-scenery-type 't_magic_sphere_cyan   "magical sphere" s_magic_sphere_cyan)
 ;; Magical spheres with light rays:
-(mk-scenery-type 't_magic_sphere_rays_red    "a magical sphere" s_magic_sphere_rays_red)
-(mk-scenery-type 't_magic_sphere_rays_green  "a magical sphere" s_magic_sphere_rays_green)
-(mk-scenery-type 't_magic_sphere_rays_blue   "a magical sphere" s_magic_sphere_rays_blue)
-(mk-scenery-type 't_magic_sphere_rays_yellow "a magical sphere" s_magic_sphere_rays_yellow)
-(mk-scenery-type 't_magic_sphere_rays_purple "a magical sphere" s_magic_sphere_rays_purple)
-(mk-scenery-type 't_magic_sphere_rays_cyan   "a magical sphere" s_magic_sphere_rays_cyan)
+(mk-scenery-type 't_magic_sphere_rays_red    "magical sphere" s_magic_sphere_rays_red)
+(mk-scenery-type 't_magic_sphere_rays_green  "magical sphere" s_magic_sphere_rays_green)
+(mk-scenery-type 't_magic_sphere_rays_blue   "magical sphere" s_magic_sphere_rays_blue)
+(mk-scenery-type 't_magic_sphere_rays_yellow "magical sphere" s_magic_sphere_rays_yellow)
+(mk-scenery-type 't_magic_sphere_rays_purple "magical sphere" s_magic_sphere_rays_purple)
+(mk-scenery-type 't_magic_sphere_rays_cyan   "magical sphere" s_magic_sphere_rays_cyan)
 ;; Arrows and Bolts and such in flight:
-(mk-scenery-type 't_arrow_wood_red    "an arrow" s_arrow_wood_red)
-(mk-scenery-type 't_arrow_metal_red   "an arrow" s_arrow_metal_red)
-(mk-scenery-type 't_arrow_wood_green  "an arrow" s_arrow_wood_green)
-(mk-scenery-type 't_arrow_metal_green "an arrow" s_arrow_metal_green)
+(mk-scenery-type 't_arrow_wood_red    "arrow" s_arrow_wood_red)
+(mk-scenery-type 't_arrow_metal_red   "arrow" s_arrow_metal_red)
+(mk-scenery-type 't_arrow_wood_green  "arrow" s_arrow_wood_green)
+(mk-scenery-type 't_arrow_metal_green "arrow" s_arrow_metal_green)
 
 (kern-mk-place 'p_sprite_gallery_1 ; tag
                "Sprite Gallery 1"  ; name
@@ -153,7 +153,7 @@
                nil ; subplaces
                nil ; neighbors
                (list
-                (list (mk-perm-gate 'p_terrain_test 21 2)  9 38)
+                (list (mk-perm-gate 'p_terrain_test 21 2)  9  1)
 
                 ;; Lightning bolts:
                 (list (kern-mk-obj t_lightning_bolt_red       1)  1  2)
@@ -197,51 +197,51 @@
                 (list (kern-mk-obj t_magic_field_dark_grey 1)  5 12)
                 (list (kern-mk-obj t_magic_field_blue_2    1)  5 13)
 
-                (list (kern-mk-obj t_magic_field_red_pulsed       1)  5 15)
-                (list (kern-mk-obj t_magic_field_green_pulsed     1)  5 16)
-                (list (kern-mk-obj t_magic_field_blue_pulsed      1)  5 17)
-                (list (kern-mk-obj t_magic_field_white_pulsed     1)  5 18)
+                (list (kern-mk-obj t_magic_field_red_pulsed       1)  7  2)
+                (list (kern-mk-obj t_magic_field_green_pulsed     1)  7  3)
+                (list (kern-mk-obj t_magic_field_blue_pulsed      1)  7  4)
+                (list (kern-mk-obj t_magic_field_white_pulsed     1)  7  5)
 
-                (list (kern-mk-obj t_magic_field_purple_pulsed    1)  7 15)
-                (list (kern-mk-obj t_magic_field_brown_pulsed     1)  7 16)
-                (list (kern-mk-obj t_magic_field_blue_2_pulsed    1)  7 17)
-                (list (kern-mk-obj t_magic_field_chromatic_pulsed 1)  7 18)
+                (list (kern-mk-obj t_magic_field_purple_pulsed    1)  7  7)
+                (list (kern-mk-obj t_magic_field_brown_pulsed     1)  7  8)
+                (list (kern-mk-obj t_magic_field_blue_2_pulsed    1)  7  9)
+                (list (kern-mk-obj t_magic_field_chromatic_pulsed 1)  7 10)
 
                 ;; Lightning discharges:
-                (list (kern-mk-obj t_lightning_discharge_red   1)  7  2)
-                (list (kern-mk-obj t_lightning_discharge_green 1)  7  3)
-                (list (kern-mk-obj t_lightning_discharge_blue  1)  7  4)
-                (list (kern-mk-obj t_lightning_discharge_black 1)  7  5)
+                (list (kern-mk-obj t_lightning_discharge_red   1)  9  2)
+                (list (kern-mk-obj t_lightning_discharge_green 1)  9  3)
+                (list (kern-mk-obj t_lightning_discharge_blue  1)  9  4)
+                (list (kern-mk-obj t_lightning_discharge_black 1)  9  5)
 
                 ;; Lightning discharges in cloud:
-                (list (kern-mk-obj t_lightning_cloud_red    1)  7  7)
-                (list (kern-mk-obj t_lightning_cloud_green  1)  7  8)
-                (list (kern-mk-obj t_lightning_cloud_blue   1)  7  9)
-                (list (kern-mk-obj t_lightning_cloud_purple 1)  7 10)
-                (list (kern-mk-obj t_lightning_cloud_yellow 1)  7 11)
-                (list (kern-mk-obj t_lightning_cloud_orange 1)  7 12)
+                (list (kern-mk-obj t_lightning_cloud_red    1)  9  7)
+                (list (kern-mk-obj t_lightning_cloud_green  1)  9  8)
+                (list (kern-mk-obj t_lightning_cloud_blue   1)  9  9)
+                (list (kern-mk-obj t_lightning_cloud_purple 1)  9 10)
+                (list (kern-mk-obj t_lightning_cloud_yellow 1)  9 11)
+                (list (kern-mk-obj t_lightning_cloud_orange 1)  9 12)
 
                 ;; Magical spheres:
-                (list (kern-mk-obj t_magic_sphere_red    1)  9  2)
-                (list (kern-mk-obj t_magic_sphere_green  1)  9  3)
-                (list (kern-mk-obj t_magic_sphere_blue   1)  9  4)
-                (list (kern-mk-obj t_magic_sphere_yellow 1)  9  5)
-                (list (kern-mk-obj t_magic_sphere_purple 1)  9  6)
-                (list (kern-mk-obj t_magic_sphere_cyan   1)  9  7)
+                (list (kern-mk-obj t_magic_sphere_red    1) 11  2)
+                (list (kern-mk-obj t_magic_sphere_green  1) 11  3)
+                (list (kern-mk-obj t_magic_sphere_blue   1) 11  4)
+                (list (kern-mk-obj t_magic_sphere_yellow 1) 11  5)
+                (list (kern-mk-obj t_magic_sphere_purple 1) 11  6)
+                (list (kern-mk-obj t_magic_sphere_cyan   1) 11  7)
 
                 ;; Magical spheres with light rays:
-                (list (kern-mk-obj t_magic_sphere_rays_red    1)  9  9)
-                (list (kern-mk-obj t_magic_sphere_rays_green  1)  9 10)
-                (list (kern-mk-obj t_magic_sphere_rays_blue   1)  9 11)
-                (list (kern-mk-obj t_magic_sphere_rays_yellow 1)  9 12)
-                (list (kern-mk-obj t_magic_sphere_rays_purple 1)  9 13)
-                (list (kern-mk-obj t_magic_sphere_rays_cyan   1)  9 14)
+                (list (kern-mk-obj t_magic_sphere_rays_red    1) 11  9)
+                (list (kern-mk-obj t_magic_sphere_rays_green  1) 11 10)
+                (list (kern-mk-obj t_magic_sphere_rays_blue   1) 11 11)
+                (list (kern-mk-obj t_magic_sphere_rays_yellow 1) 11 12)
+                (list (kern-mk-obj t_magic_sphere_rays_purple 1) 11 13)
+                (list (kern-mk-obj t_magic_sphere_rays_cyan   1) 11 14)
 
                 ;; Arrows and Bolts and such in flight:
-                (list (kern-mk-obj t_arrow_wood_red    1)  9 16)
-                (list (kern-mk-obj t_arrow_metal_red   1)  9 17)
-                (list (kern-mk-obj t_arrow_wood_green  1) 10 16)
-                (list (kern-mk-obj t_arrow_metal_green 1) 10 17)
+                (list (kern-mk-obj t_arrow_wood_red    1) 13  2)
+                (list (kern-mk-obj t_arrow_metal_red   1) 13  3)
+                (list (kern-mk-obj t_arrow_wood_green  1) 13  4)
+                (list (kern-mk-obj t_arrow_metal_green 1) 13  5)
                 
                 ) ; objects
                nil ; hooks
@@ -308,7 +308,7 @@
 
                nil ; neighbors
                (list
-                (list (mk-perm-gate 'p_sprite_gallery_1  9 38) 21  2)
+                (list (mk-perm-gate 'p_sprite_gallery_1  9  1) 21  2)
 
                 ) ; objects
                nil ; hooks
