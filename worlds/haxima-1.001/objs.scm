fff020eefd60378832527f941aac5b2c52f33951
Sam Glasby <sglasby@users.sourceforge.net>
2004-10-09 20:27:16 +0000

    Added some new chest types.

--- worlds/haxima-1.001/objs.scm
+++ worlds/haxima-1.001/objs.scm
@@ -15,8 +15,18 @@
                       (kern-obj-put-into kobj getter)))))
 
 ;; Make a simple container (no ifc)
-(mk-obj-type 't_small_wooden_chest "small wooden chest" s_chest_small_wooden_closed
+(mk-obj-type 't_small_wooden_chest 
+             "small wooden chest"  s_chest_small_wooden_closed
              layer-container nil)
 
-(mk-obj-type 't_large_wooden_chest "large wooden chest" s_chest_large_wooden_closed
+(mk-obj-type 't_large_wooden_chest 
+             "large wooden chest"  s_chest_large_wooden_closed
+             layer-container nil)
+
+(mk-obj-type 't_small_iron_chest 
+             "small iron chest"   s_chest_small_metal_1_closed
+             layer-container nil)
+
+(mk-obj-type 't_large_iron_chest 
+             "large iron chest"   s_chest_large_metal_1_closed
              layer-container nil)
