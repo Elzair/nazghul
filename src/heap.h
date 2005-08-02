6dbe25101ef83189018f5e0dfecd1fb3550d48e6
Gordon McNutt <gmcnutt@users.sourceforge.net>
2005-08-02 13:40:38 +0000

    Cleaned up header

--- src/heap.h
+++ src/heap.h
@@ -22,30 +22,29 @@
 #ifndef heap_h
 #define heap_h
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include "macros.h"
+
+BEGIN_DECL
 
 #define heap_entry(ptr,type,field) \
         ((type*)((char*)(ptr)-(unsigned long)(&((type *)0)->field)))
 
 #define heap_empty(h) (!(h)->num_entries)
 
-	struct heap {
-		unsigned int max_entries;
-		unsigned int num_entries;
-		int **entries;
-	};
+struct heap {
+        unsigned int max_entries;
+        unsigned int num_entries;
+        int **entries;
+};
 
-	extern struct heap *heap_create(unsigned int max_entries);
-	extern void heap_destroy(struct heap *heap);
-	extern void heapify(struct heap *heap, int i);
-	extern int heap_expand(struct heap *heap);
-	extern int heap_insert(struct heap *heap, int *entry);
-	extern int *heap_extract(struct heap *heap);
-	extern void heap_clean(struct heap *heap);
+extern struct heap *heap_create(unsigned int max_entries);
+extern void heap_destroy(struct heap *heap);
+extern void heapify(struct heap *heap, int i);
+extern int heap_expand(struct heap *heap);
+extern int heap_insert(struct heap *heap, int *entry);
+extern int *heap_extract(struct heap *heap);
+extern void heap_clean(struct heap *heap);
 
-#ifdef __cplusplus
-}
-#endif
+END_DECL
+
 #endif
