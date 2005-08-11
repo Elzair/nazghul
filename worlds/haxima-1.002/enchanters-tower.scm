744652bef9ed91119be93b497bd0a32ef6864a95
Gordon McNutt <gmcnutt@users.sourceforge.net>
2005-08-11 03:20:46 +0000

    Haxima changes: o Tweaks to enchtwr

--- worlds/haxima-1.002/enchanters-tower.scm
+++ worlds/haxima-1.002/enchanters-tower.scm
@@ -6,37 +6,37 @@
 (kern-mk-map 
  'm_enchanters_tower 31 31 pal_expanded
  (list
-  "tt tt bb tt bb || || tt %% %% %% %% %% %% .. .. .. %% %% %% %% %% %% %% %% %% %% %% %% %% %% "
-  "bb tt .. .. tt tt || || tt tt %% %% %% bb .. .. .. bb .. bb .. bb .. bb .. bb .. bb .. %% %% "
-  "tt .. .. .. .. .. bb || || tt %% %% %% .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. %% "
-  "bb .. .. .. .. .. .. tt || || %% %% %% bb .. .. .. bb .. bb .. bb .. bb .. bb .. .. .. bb %% "
-  "|| .. .. .. .. .. .. .. bb || tt %% %% %% .. .. .. %% %% %% %% %% %% %% %% %% %% .. .. .. %% "
-  "|| bb .. .. .. .. .. .. .. || || %% %% xx w+ cc w+ xx %% tt tt tt tt tt %% %% %% bb .. bb %% "
-  "|| || .. .. .. .. && .. .. tt || xx xx xx ,, cc ,, xx xx xx || || || tt tt %% %% .. .. .. %% "
-  "tt bb .. .. bb .. .. .. .. xx w+ xx && xx ,, cc ,, xx && xx xx xx || || tt tt %% bb .. bb %% "
-  "%% .. .. || || .. .. .. xx xx ,, ,, ,, xx x! cc x! xx ,, ,, xx xx ?? || || tt %% .. .. .. %% "
-  "%% bb .. bb || tt .. xx xx ,, ,, ,, ,, xx ,, cc ,, ,, ,, ,, xx ,, ?? xx || tt %% bb .. bb %% "
-  "%% .. .. || || tt tt xx ,, ,, 00 ,, ,, xx ,, cc ,, xx xx xx xx xx xx xx || tt %% .. .. .. %% "
-  "%% bb .. bb || || xx xx ,, ,, 00 ,, ,, xx ,, cc ,, xx ,, ,, ,, ,, ,, xx xx %% %% bb .. bb %% "
-  "%% .. .. %% tt || xx ,, ,, ,, ,, ,, ,, ,, ,, cc ,, ,, ,, ,, ,, ,, ,, && xx %% %% .. .. .. %% "
-  "%% bb .. bb %% xx xx xx xx xx xx xx xx xx ,, cc ,, xx xx xx xx xx xx xx xx xx %% bb .. bb %% "
-  ".. .. .. .. .. w+ ,, ,, ,, ,, xx ,, ,, x! ,, cc ,, x! ,, ,, xx ,, ,, ,, ,, w+ .. .. .. .. .. "
-  ".. .. .. .. .. cc cc cc cc ,, xx ,, ,, ,, ,, cc ,, ,, ,, ,, xx ,, cc cc cc cc .. .. .. .. .. "
-  ".. .. .. .. .. w+ ,, ,, cc ,, xx ,, ,, xx ,, cc ,, xx ,, ,, xx ,, cc ,, ,, w+ .. .. .. .. .. "
-  "%% bb .. bb %% xx xx x! cc x! xx xx xx xx ,, cc ,, xx xx xx xx x! cc x! xx xx %% bb .. bb %% "
-  "%% %% %% %% %% %% w+ ,, cc ,, ,, ,, ,, ,, ,, cc ,, ,, ,, ,, ,, ,, cc ,, w+ tt %% %% %% %% %% "
-  "%% %% .. %% %% %% xx xx cc ,, pp ,, ,, pp ,, cc ,, pp ,, ,, pp ,, cc xx xx tt tt tt tt tt tt "
-  "%% .. .. .. %% %% %% w+ cc cc cc cc cc cc cc cc cc cc cc cc cc cc cc xx tt || || || || || tt "
-  "%% %% .. %% %% %% %% xx xx cc cc cc cc cc cc cc cc cc cc cc cc cc xx xx || || bb tt bb || || "
-  "%% %% %% %% %% %% %% %% xx xx x! ,, ,, pp ,, cc ,, pp ,, ,, x! xx xx || || tt tt .. tt tt || "
-  "%% %% tt tt tt tt .. %% %% xx xx xx ,, ,, ,, cc ,, ,, ,, xx xx xx tt || || bb .. .. .. bb || "
-  "%% tt tt ~~ ~~ tt tt .. %% %% %% xx w+ xx ,, cc ,, xx w+ xx tt tt ~~ tt || tt .. ++ .. tt || "
-  "tt tt ~~ -- -- ~~ tt tt .. %% %% %% %% xx w+ cc w+ xx %% %% tt ~~ ~~ -- || bb .. .. .. bb || "
-  "tt ~~ -- __ __ -- ~~ tt .. %% %% %% %% %% .. .. .. %% %% %% %% tt ~~ tt || || tt .. tt || || "
-  "tt ~~ -- __ __ -- ~~ tt .. %% %% %% %% bb .. .. .. bb .. bb .. bb %% %% || || bb .. bb || || "
-  "tt tt ~~ -- -- ~~ tt tt .. %% .. %% %% .. .. .. .. .. .. .. .. .. == .. tt tt .. .. tt || || "
-  "%% tt tt ~~ ~~ tt tt .. %% .. .. .. %% bb .. .. .. bb .. bb .. bb .. %% || bb tt bb || || || "
-  "%% %% tt tt tt tt .. %% %% %% .. %% %% %% .. .. .. %% %% %% %% %% %% tt || || || || || || || "
+            "tt tt bb tt bb || || tt %% %% %% %% %% %% .. .. .. %% %% %% %% %% %% %% %% %% %% %% %% %% %% "
+            "bb tt .. .. tt tt || || tt tt %% %% %% bb .. .. .. bb .. bb .. bb .. bb .. bb .. bb .. %% %% "
+            "tt .. .. .. .. .. bb || || tt %% %% %% .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. .. %% "
+            "bb .. .. .. .. .. .. tt || || %% %% %% bb .. .. .. bb .. bb .. bb .. bb .. bb .. .. .. bb %% "
+            "|| .. .. .. .. .. .. .. bb || tt %% %% %% .. .. .. %% %% %% %% %% %% %% %% %% %% .. .. .. %% "
+            "|| bb .. .. .. .. .. .. .. || || %% %% xx w+ cc w+ xx %% tt tt tt tt tt %% %% %% bb .. bb %% "
+            "|| || .. .. .. .. && .. .. tt || xx xx xx ,, cc ,, xx xx xx || || || tt tt %% %% .. .. .. %% "
+            "tt bb .. .. bb .. .. .. .. xx w+ xx && xx ,, cc ,, xx && xx xx xx || || tt tt %% bb .. bb %% "
+            "%% .. .. || || .. .. .. xx xx ,, ,, ,, xx x! cc x! xx ,, ,, xx xx ?? || || tt %% .. .. .. %% "
+            "%% bb .. bb || tt .. xx xx ,, ,, ,, ,, xx ,, cc ,, ,, ,, ,, xx ,, ?? xx || tt %% bb .. bb %% "
+            "%% .. .. || || tt tt xx ,, ,, 00 ,, ,, xx ,, cc ,, xx xx xx xx xx xx xx || tt %% .. .. .. %% "
+            "%% bb .. bb || || xx xx ,, ,, 00 ,, ,, xx ,, cc ,, xx ,, ,, ,, ,, ,, xx xx %% %% bb .. bb %% "
+            "%% .. .. %% tt || xx ,, ,, ,, ,, ,, ,, ,, ,, cc ,, ,, ,, ,, ,, ,, ,, && xx %% %% .. .. .. %% "
+            "%% bb .. bb %% xx xx xx xx xx xx xx xx xx ,, cc ,, xx xx xx xx xx xx xx xx xx %% bb .. bb %% "
+            ".. .. .. .. .. w+ ,, ,, ,, ,, xx ,, ,, x! ,, cc ,, x! ,, ,, xx ,, ,, ,, ,, w+ .. .. .. .. .. "
+            ".. .. .. .. .. cc cc cc cc ,, xx ,, ,, ,, ,, cc ,, ,, ,, ,, xx ,, cc cc cc cc .. .. .. .. .. "
+            ".. .. .. .. .. w+ ,, ,, cc ,, xx ,, ,, xx ,, cc ,, xx ,, ,, xx ,, cc ,, ,, w+ .. .. .. .. .. "
+            "%% bb .. bb %% xx xx x! cc x! xx xx xx xx ,, cc ,, xx xx xx xx x! cc x! xx xx %% bb .. bb %% "
+            "%% %% %% %% %% %% w+ ,, cc ,, ,, ,, ,, ,, ,, cc ,, ,, ,, ,, ,, ,, cc ,, w+ tt %% %% %% %% %% "
+            "%% %% .. %% %% %% xx xx cc ,, pp ,, ,, pp ,, cc ,, pp ,, ,, pp ,, cc xx xx tt tt tt tt tt tt "
+            "%% .. .. .. %% %% %% w+ cc cc cc cc cc cc cc cc cc cc cc cc cc cc cc xx tt || || || || || tt "
+            "%% %% .. %% %% %% %% xx xx cc cc cc cc cc cc cc cc cc cc cc cc cc xx xx || || bb tt bb || || "
+            "%% %% %% %% %% %% %% %% xx xx x! ,, ,, pp ,, cc ,, pp ,, ,, x! xx xx || || tt tt .. tt tt || "
+            "%% %% tt tt tt tt .. %% %% xx xx xx ,, ,, ,, cc ,, ,, ,, xx xx xx tt || || bb .. .. .. bb || "
+            "%% tt tt ~~ ~~ tt tt .. %% %% %% xx w+ xx xx cc xx xx w+ xx %% %% ~~ tt || tt .. ++ .. tt || "
+            "tt tt ~~ -- -- ~~ tt tt .. %% %% %% %% xx ,, cc ,, xx %% %% %% ~~ ~~ -- || bb .. .. .. bb || "
+            "tt ~~ -- __ __ -- ~~ tt .. %% %% %% %% xx ,, cc ,, xx %% %% %% %% ~~ %% || || tt .. tt || || "
+            "tt ~~ -- __ __ -- ~~ tt .. %% %% %% %% xx w+ cc w+ xx .. bb .. bb %% %% || || bb .. bb || || "
+            "tt tt ~~ -- -- ~~ tt tt .. %% .. %% %% .. .. .. .. .. .. .. .. .. == .. tt tt .. .. tt || || "
+            "%% tt tt ~~ ~~ tt tt .. %% .. .. .. %% bb .. .. .. bb .. bb .. bb %% %% || bb tt bb || || || "
+            "%% %% tt tt tt tt .. %% %% %% .. %% %% %% .. .. .. %% %% %% %% %% %% %% || || || || || || || "
   ))
 
 ;;----------------------------------------------------------------------------
@@ -73,17 +73,18 @@
   ;; characters
   (list ch_zane 0 0) 
 
-  (put (mk-door) 5 15)
-  (put (mk-door) 25 15)
-  (put (mk-door) 15 25)
-  (put (mk-door) 15 5)
+  (put (mk-portcullis) 5 15)
+  (put (mk-portcullis) 25 15)
+  (put (mk-portcullis) 15 27)
+  (put (mk-portcullis) 15 5)
 
   (put (mk-portcullis) 13 15)
   (put (mk-portcullis) 17 15)
 
-  (put (mk-portcullis) 15  8)
-  (put (mk-portcullis) 22 17)
-  (put (mk-portcullis)  8 17)
+  (put (mk-door) 15  8)
+  (put (mk-door) 15 24)
+  (put (mk-door) 22 17)
+  (put (mk-door)  8 17)
 
   (put (mk-door) 13 12)
   (put (mk-door) 17  9)
