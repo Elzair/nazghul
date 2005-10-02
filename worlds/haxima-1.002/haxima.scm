bb5a43a6fbf0b814b124076513a47ef116156e04
Gordon McNutt <gmcnutt@users.sourceforge.net>
2005-10-02 19:37:20 +0000

    Haxima changes: o Added map and objects for the Necromancers Lair

--- worlds/haxima-1.002/haxima.scm
+++ worlds/haxima-1.002/haxima.scm
@@ -44,6 +44,7 @@
 (load "old-absalot.scm")
 (load "engineers-hut.scm")
 (load "mans-hideout.scm")
+(load "necromancers-lair.scm")
 
 ;;----------------------------------------------------------------------------
 ;; Characters
@@ -235,8 +236,8 @@
   ;(kern-log-msg "You notice a cave to the southwest."))
   ;(kern-obj-put-at kplayer (list p_slimy_cavern 13 2)
   ;(kern-obj-put-at kplayer (list p_enchanters_tower 32 32)))
-  (kern-obj-put-at kplayer (list p_shard 51 7)))
-  ;(kern-obj-put-at kplayer (list p_mans_hideout 9 18)))
+  ;(kern-obj-put-at kplayer (list p_shard 51 7)))
+  (kern-obj-put-at kplayer (list p_necromancers_lair 9 9)))
   ;(kern-obj-put-at kplayer (list p_troll_den 9 0)))                  
 ;;----------------------------------------------------------------------------
 ;; To skip the extended start scene comment out this next line and uncomment
---
+++ worlds/haxima-1.002/necromancers-lair.scm
@@ -0,0 +1,103 @@
+;;----------------------------------------------------------------------------
+;; Map
+;;----------------------------------------------------------------------------
+(kern-mk-map
+ 'm_necromancers_lair 19 19 pal_expanded
+ (list
+      "xx xx xx xx xx xx xx xx xx && xx xx xx xx xx x! xx xx xx "
+      "xx xx ,, ,, ,, xx xx xx ,, ,, ,, xx xx xx ,, ,, ,, xx xx "
+      "xx ,, ,, ,, ,, ,, xx ,, ,, ,, ,, ,, xx ,, ,, ,, ,, ,, xx "
+      "xx ,, ,, x! ,, ,, ,, ,, [[ @@ ]] ,, xx ,, ,, ,, ,, ,, xx "
+      "xx ,, ,, ,, ,, ,, xx ,, ,, ,, ,, ,, xx ,, ,, ,, ,, ,, xx "
+      "xx xx ,, ,, ,, xx xx xx ,, ,, ,, xx xx xx ,, ,, ,, xx xx "
+      "xx xx xx xx xx xx xx xx x! ,, x! xx xx xx x! ,, x! xx xx "
+      "xx xx .. .. .. xx xx xx ,, ,, ,, xx xx xx ,, ,, ,, xx xx "
+      "xx .. .. .. ,, .. xx ,, ,, ,, ,, ,, x! ,, ,, ,, ,, ,, xx "
+      "xx .. ,, ,, .. .. xx ,, ,, ,, ,, ,, ,, ,, ,, ,, ,, ,, xx "
+      "xx .. .. ,, .. .. xx ,, ,, ,, ,, ,, x! ,, ,, ,, ,, ,, xx "
+      "xx xx .. .. .. xx xx xx ,, ,, ,, xx xx xx ,, ,, ,, xx xx "
+      "xx xx xx ,, xx xx xx xx x! ,, x! xx xx xx x! ,, x! xx xx "
+      "xx xx ,, ,, ,, xx xx xx ,, ,, ,, xx xx xx ,, ,, ,, xx xx "
+      "xx ,, ,, ,, ,, ,, x! ,, ~~ ee ~~ ~~ xx 00 ,, ,, ,, 00 xx "
+      "xx ,, ,, ,, ,, ,, ,, ,, ee ee ~~ ~~ xx 00 ,, ,, ,, 00 xx "
+      "xx ,, ,, ,, ,, ,, x! ,, ~~ ~~ ~~ ~~ xx 00 ,, ,, ,, 00 xx "
+      "xx x! [[ @@ ]] x! xx xx ~~ ~~ ~~ xx xx x! 00 00 00 x! xx "
+      "xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx xx "
+))
+
+;;----------------------------------------------------------------------------
+;; NPC's
+;;----------------------------------------------------------------------------
+
+
+;;----------------------------------------------------------------------------
+;; Place
+;;----------------------------------------------------------------------------
+(kern-mk-place
+ 'p_necromancers_lair     ; tag
+ "Necromancers Lair" ; name
+ nil      ; sprite
+ m_necromancers_lair      ; map
+ #f              ; wraps
+ #t              ; underground
+ #f              ; large-scale (wilderness)
+ #f              ; tmp combat place
+ nil ; subplaces
+ nil ; neighbors
+
+ (list ; objects
+
+  ;; bedroom
+  (put (mk-bed) 15 3)
+  (put (mk-door) 15 6)
+
+  ;; galley
+  (put (mk-chest
+        nil
+        (list (list 5 t_food))) 10 1)
+
+  ;; library
+  (put (mk-door) 15 12)
+
+  ;; center
+  (put (mk-door) 9 12)
+  (put (mk-door) 9 6)
+  (put (mk-door) 12 9)
+
+  ;; lab
+  (put (mk-door) 6 15)
+  (put (mk-locked-door) 3 12)
+
+  ;; morgue
+  (put (mk-corpse) 2 7)
+  (put (mk-corpse) 3 7)
+  (put (mk-corpse) 4 7)
+  (put (mk-corpse) 1 8)
+  (put (mk-corpse) 2 8)
+  (put (mk-corpse) 5 8)
+  (put (mk-corpse) 1 10)
+  (put (mk-corpse) 5 9)
+
+  ;; potion room
+  (put (mk-magic-locked-door) 6 3)
+  (put (kern-mk-obj mandrake 6) 2 1)
+  (put (kern-mk-obj nightshade 4) 3 1)
+  (put (kern-mk-obj blood_moss 12) 4 1)
+  (put (kern-mk-obj black_pearl 14) 2 5)
+  (put (kern-mk-obj spider_silk 21) 3 5)
+  (put (kern-mk-obj garlic 18) 4 5)
+  (put (kern-mk-obj ginseng 17) 1 4)
+  (put (kern-mk-obj sulphorous_ash 26) 1 3)
+  (put (kern-mk-obj t_mana_potion 8) 1 2)
+  )
+
+ nil ; hooks
+ (list  ;; edge entrances
+  (list east  0 9)
+  (list south 9 0) 
+  (list north 9 18)
+  (list west  18 9)
+  )
+ )
+
+
