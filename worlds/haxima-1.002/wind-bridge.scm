2cfb20b354d0c20e0da84a7dfe53cf2342b8a3dc
Gordon McNutt <gmcnutt@users.sourceforge.net>
2006-11-29 01:36:03 +0000

    Wind bridge now delays two turns before rotating

--- worlds/haxima-1.002/wind-bridge.scm
+++ worlds/haxima-1.002/wind-bridge.scm
@@ -1,6 +1,7 @@
 ;; A wind bridge appears to rotates in the wind. It blits a map to lay down the
 ;; bridge terrain. Position the object in the upper left corner of where the
-;; bridge map should be blitted.
+;; bridge map should be blitted. The bridge will delay a few turns before
+;; turning, giving the player some time to get to safety.
 
 (kern-mk-map
  'm_wind_bridge_ns 9 9 pal_expanded
@@ -30,9 +31,12 @@
   "vv vv vv vv vv vv vv vv vv "
   ))
 
-(define (wind-bridge-mk) (list here))
+(define (wind-bridge-mk) (list here 0))
 (define (wind-bridge-facing gob) (car gob))
+(define (wind-bridge-delay gob) (cadr gob))
 (define (wind-bridge-set-facing! gob val) (set-car! gob val))
+(define (wind-bridge-inc-delay! gob) (set-car! (cdr gob) (+ 1 (cadr gob))))
+(define (wind-bridge-reset-delay! gob) (set-car! (cdr gob) 0))
 
 (define (wind-bridge-exec kobj)
   (let ((wind (kern-get-wind))
@@ -40,23 +44,31 @@
         (loc (kern-obj-get-location kobj))
         )
     ;;(println "wind=" wind "gob=" gob "loc=" loc)
-    (define (blit map)
-      (kern-blit-map (kern-place-map (loc-place loc))
-                     (loc-x loc)
-                     (loc-y loc)
-                     map 0 0 9 9))
+    (define (turn map dir)
+      (cond ((< (wind-bridge-delay gob) 2)
+             (kern-log-msg "The bridge creaks in the wind!")
+             (wind-bridge-inc-delay! gob))
+            (else
+             (kern-blit-map (kern-place-map (loc-place loc))
+                            (loc-x loc)
+                            (loc-y loc)
+                            map 0 0 9 9)
+             (wind-bridge-set-facing! gob dir)
+             (wind-bridge-reset-delay! gob)
+             )))
     (cond ((and (or (= wind north)
                     (= wind south))
                 (not (= (wind-bridge-facing gob) north)))
-           (blit m_wind_bridge_ns)
-           (wind-bridge-set-facing! gob north)
+           (turn m_wind_bridge_ns north)
            )
           ((and (or (= wind east)
                     (= wind west))
                 (not (= (wind-bridge-facing gob) east)))
-           (blit m_wind_bridge_ew)
-           (wind-bridge-set-facing! gob south)
-           ))))
+           (turn m_wind_bridge_ew east)
+           )
+          (else
+           (wind-bridge-reset-delay! gob)
+          ))))
 
 (define wind-bridge-ifc
   (ifc '()
