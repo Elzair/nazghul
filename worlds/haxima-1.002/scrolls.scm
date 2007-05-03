2134f70614c49ba5f16658e5c5fc927796b2ff30
Kris Parker <kaypy@users.sourceforge.net>
2007-05-03 11:31:54 +0000

    bug 1666217 scrolls now use their own context checking (for the important ones anyway, Ill deal with the rest some other time)

--- worlds/haxima-1.002/scrolls.scm
+++ worlds/haxima-1.002/scrolls.scm
@@ -26,23 +26,58 @@
                   (lambda (kscrolltype kuser)
                     (apply spell (list kuser)))))
 
-(mk-scroll 't_an_tym_scroll "An Tym scroll" s_an_tym_scroll an-tym)
-(mk-scroll 't_in_mani_corp_scroll "In Mani Corp scroll" s_in_mani_corp_scroll in-mani-corp)
-(mk-scroll 't_vas_rel_por_scroll "Vas Rel Por scroll" s_vas_rel_por_scroll vas-rel-por)
-(mk-scroll 't_xen_corp_scroll "Xen Corp scroll" s_xen_corp_scroll xen-corp)
-(mk-scroll 't_sanct_lor_scroll "Sanct Lor scroll" s_sanct_lor_scroll sanct-lor)
-(mk-scroll 't_in_quas_xen_scroll "In Quas Xen scroll" s_in_quas_xen_scroll in-quas-xen)
-(mk-scroll 't_in_vas_por_ylem_scroll "In Vas Por Ylem scroll" s_in_vas_por_ylem_scroll in-vas-por-ylem)
-(mk-scroll 't_an_xen_ex_scroll "An Xen Ex scroll" s_an_xen_ex_scroll an-xen-ex)
-(mk-scroll 't_in_an_scroll "In An scroll" s_in_an_scroll in-an)
-(mk-scroll 't_in_ex_por_scroll "In Ex Por scroll" s_in_ex_por_scroll in-ex-por)
-(mk-scroll 't_vas_mani_scroll "Vas Mani scroll" s_vas_mani_scroll vas-mani)
-(mk-scroll 't_wis_quas_scroll "Wis Quas scroll" s_wis_quas_scroll wis-quas)
-(mk-scroll 't_wis_an_ylem_scroll "Wis An Ylem scroll" s_wis_an_ylem_scroll wis-an-ylem)
+(define (player-in-wilderness?)
+	(kern-place-is-wilderness? (loc-place (kern-obj-get-location (kern-get-player))))
+	)
 
-;; Temp shims to keep saved games working
-;; Roll saved games with the new 0.6.x series
-;;(define t_an_xen_exe_scroll t_an_xen_ex_scroll)
-;;(define t_in_exe_por_scroll t_in_ex_por_
-;;Note: bug in scheme interpreter: w/o a newline below the next file (keys.scm,
-;;currently) will not be loaded properly.
+;;-----------------------------------------------------------------------------------------
+;; Scroll Functions
+;;-----------------------------------------------------------------------------------------
+
+(define (scroll-xen-corp caster)
+	(if (player-in-wilderness?)
+		result-not-here
+		(user-cast-ranged-targeted-spell caster 4 cast-kill-proc)
+	))
+		
+(define (scroll-in-quas-xen caster)
+	(if (player-in-wilderness?)
+		result-not-here
+		(cast-ui-basic-ranged-spell powers-clone
+			caster 
+			(powers-clone-range 12)
+			12)
+	))
+
+(define (scroll-in-vas-por-ylem caster)
+	(if (player-in-wilderness?)
+		result-not-here
+		(powers-tremor caster caster 12)
+	))
+
+(define (scroll-an-xen-ex caster)
+	(if (player-in-wilderness?)
+		result-not-here
+		(cast-ui-basic-ranged-spell powers-charm
+			caster 
+			(powers-charm-range 12)
+			12)
+	))
+		
+;;-----------------------------------------------------------------------------------------
+;; Scroll List
+;;-----------------------------------------------------------------------------------------
+
+(mk-scroll 't_an_tym_scroll "An Tym scroll" s_an_tym_scroll an-tym) ;; context-any
+(mk-scroll 't_in_mani_corp_scroll "In Mani Corp scroll" s_in_mani_corp_scroll in-mani-corp)  ;; context-any
+(mk-scroll 't_vas_rel_por_scroll "Vas Rel Por scroll" s_vas_rel_por_scroll vas-rel-por)  ;; context-any
+(mk-scroll 't_xen_corp_scroll "Xen Corp scroll" s_xen_corp_scroll scroll-xen-corp) 
+(mk-scroll 't_sanct_lor_scroll "Sanct Lor scroll" s_sanct_lor_scroll sanct-lor) ;; context-any
+(mk-scroll 't_in_quas_xen_scroll "In Quas Xen scroll" s_in_quas_xen_scroll scroll-in-quas-xen)
+(mk-scroll 't_in_vas_por_ylem_scroll "In Vas Por Ylem scroll" s_in_vas_por_ylem_scroll scroll-in-vas-por-ylem) 
+(mk-scroll 't_an_xen_ex_scroll "An Xen Ex scroll" s_an_xen_ex_scroll scroll-an-xen-ex)
+(mk-scroll 't_in_an_scroll "In An scroll" s_in_an_scroll in-an) ;; context-any
+(mk-scroll 't_in_ex_por_scroll "In Ex Por scroll" s_in_ex_por_scroll in-ex-por)  ;; context-any??
+(mk-scroll 't_vas_mani_scroll "Vas Mani scroll" s_vas_mani_scroll vas-mani) ;; context-any
+(mk-scroll 't_wis_quas_scroll "Wis Quas scroll" s_wis_quas_scroll wis-quas) ;; context-any
+(mk-scroll 't_wis_an_ylem_scroll "Wis An Ylem scroll" s_wis_an_ylem_scroll wis-an-ylem) ;; context-any
