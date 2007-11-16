edb941be349f9ccfdb646197c85df9e5a5319ae4
Gordon McNutt <gmcnutt@users.sourceforge.net>
2007-11-16 13:20:26 +0000

    Blended the demo map.

--- worlds/haxima-1.002/demo.scm
+++ worlds/haxima-1.002/demo.scm
@@ -1587,27 +1587,28 @@
 ;;----------------------------------------------------------------------------
 (kern-mk-map
  'm_demo_scene 19 19 pal_expanded
- (list
+
+	(list
       "000 001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018 "
-      "019 020 021 022 023 024 025 026 027 028 029 030 031 032 033 034 035 036 037 "
-      "038 039 040 041 042 043 044 045 046 047 048 049 050 051 052 053 054 055 056 "
-      "057 058 059 060 061 062 063 064 065 066 067 068 069 070 071 072 073 074 075 "
-      "076 077 078 079 080 081 082 083 084 085 086 087 088 089 090 091 092 093 094 "
-      "095 096 097 098 099 100 101 102 103 104 105 106 107 108 109 110 111 112 113 "
-      "fg fh fh fh fh fh fh fh fh fh fh fh fh fh fh fh fh fh fi "
-      "fj tt tt .. .. .. .. .. .. .. .. tt tt tt tt ~* tt tt fl "
-      "fj tt tt .. ar .. .. .. ar .. .. .. tt tt ~* ~* tt tt fl "
-      "fj .. .. .. .. .. .. .. .. .. .. .. {f tt ~* tt tt tt fl "
-      "fj .. ar .. .. .. .. .. .. .. ar .. .. .. ~* .. .. .. fl "
-      "fj .. .. .. .. .. dd .. .. .. .. .. dd ee ee ee dd dd fl "
-      "fj tt .. .. .. dd dd dd dd dd dd dd dd ee ee ee dd dd fl "
-      "fj .. .. .. .. .. dd .. .. .. .. .. dd ee ee ee dd dd fl "
-      "fj .. ar .. .. .. .. .. .. .. ar .. .. .. ~* .. .. .. fl "
-      "fj .. .. .. .. .. .. .. .. .. .. .. tt tt ~* tt tt tt fl "
-      "fj tt tt .. ar .. .. .. ar .. .. .. tt tt ~* ~* tt tt fl "
-      "fj tt tt .. .. .. .. .. .. .. .. tt tt tt tt ~* tt tt fl "
-      "fm fn fn fn fn fn fn fn fn fn fn fn fn fn fn fn fn fn fo "
-  )
+		"019 020 021 022 023 024 025 026 027 028 029 030 031 032 033 034 035 036 037 "
+		"038 039 040 041 042 043 044 045 046 047 048 049 050 051 052 053 054 055 056 "
+		"057 058 059 060 061 062 063 064 065 066 067 068 069 070 071 072 073 074 075 "
+		"076 077 078 079 080 081 082 083 084 085 086 087 088 089 090 091 092 093 094 "
+		"095 096 097 098 099 100 101 102 103 104 105 106 107 108 109 110 111 112 113 "
+		"fg fh fh fh fh fh fh fh fh fh fh fh fh fh fh fh fh fh fi "
+		"fj tt tt .. .. .. .. .. .. .. .. tt tt tt tt __ tt tt fl "
+		"fj tt t# .. ar .. .. .. ar .. .. t% tt tt _3 _c tt tt fl "
+		"fj .. .. .. .. .. .. .. .. .. .. .. ta tc _2 tG tt tt fl "
+		"fj .. ar .. .. .. .. .. .. .. ar .. .. .. __ .. .. .. fl "
+		"fj .. .. .. .. .. dd .. .. .. .. .. dd ee ee ee dd dd fl "
+		"fj .. .. .. .. dd {f dd dd dd dd dd dd ee ee ee dd dd fl "
+		"fj .. .. .. .. .. dd .. .. .. .. .. dd ee ee ee dd dd fl "
+		"fj .. ar .. .. .. .. .. .. .. ar .. .. .. __ .. .. .. fl "
+		"fj .. .. .. .. .. .. .. .. .. .. .. t3 t5 _2 tJ tt tt fl "
+		"fj tt tA .. ar .. .. .. ar .. .. tC tt tt _a _5 tt tt fl "
+		"fj tt tt .. .. .. .. .. .. .. .. tt tt tt tt __ tt tt fl "
+		"fm fn fn fn fn fn fn fn fn fn fn fn fn fn fn fn fn fn fo "
+	)
  )
 
 (kern-mk-place
