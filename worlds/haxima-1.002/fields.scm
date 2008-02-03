9a0aa75df932fe1e3706c7533e9050c1e20b96c6
Kris Parker <kaypy@users.sourceforge.net>
2008-02-03 14:03:29 +0000

    Added acid fields (I just needed something yellow, really)

--- worlds/haxima-1.002/fields.scm
+++ worlds/haxima-1.002/fields.scm
@@ -23,13 +23,16 @@
 (kern-mk-field-type 'F_poison "poison field" s_field_poison   256  20 pclass-none  'apply-poison	mmode-field)
 (kern-mk-field-type 'F_sleep  "sleep field"  s_field_sleep    256  20 pclass-none  'apply-sleep	mmode-field)
 (kern-mk-field-type 'F_energy "energy field" s_field_energy   512  20 pclass-repel 'apply-lightning	mmode-field)
+(kern-mk-field-type 'F_acid "acid field" s_field_acid   256  20 pclass-none 'apply-acid	mmode-field)
 (kern-mk-field-type 'web-type "spider web"   s_spider_web     0    20 pclass-none  'ensnare	mmode-field)
 
 (kern-mk-field-type 'F_poison_perm "poison field" s_field_poison 256 -1 pclass-none  'apply-poison	mmode-field)
 (kern-mk-field-type 'F_sleep_perm  "sleep field"  s_field_sleep  256 -1 pclass-none  'apply-sleep	mmode-field)
 (kern-mk-field-type 'F_energy_perm "energy field" s_field_energy 512 -1 pclass-repel 'apply-lightning	mmode-field)
 (kern-mk-field-type 'F_fire_perm   "fire field"   s_field_fire   512 -1 pclass-none  'burn	mmode-field)
+(kern-mk-field-type 'F_acid_perm "acid field" s_field_acid 256 -1 pclass-none 'apply-acid	mmode-field)
 (kern-mk-field-type 'F_web_perm    "spider web"   s_spider_web   0   -1 pclass-none  'ensnare	mmode-field)
+(kern-mk-field-type 'F_illum_perm    nil   nil   256   -1 pclass-none  nil	mmode-field)
 
 (define all-field-types
   (list F_fire F_poison F_sleep F_energy web-type
