ac82febc6bb14eb7d2bd7f286301c834056fd118
Kris Parker <kaypy@users.sourceforge.net>
2008-11-11 15:00:14 +0000

    Lets try Ogg music

--- worlds/haxima-1.002/sounds.scm
+++ worlds/haxima-1.002/sounds.scm
@@ -44,7 +44,7 @@
 ;;==========================================================================
 ;; music
 
-(define default-music "ballad.mid")
+(define default-music "ballad.ogg")
 
 (define (music-list . entries)
 	(if (null? entries)
