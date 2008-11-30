759df8031f35db02857a9ef895f466a308b90bf3
Gordon McNutt <gmcnutt@users.sourceforge.net>
2008-11-30 20:44:37 +0000

    Part 1 of moving the current terrain and quick terrain settings from the palette struct to the terrain editor code

--- src/terrain_editor.c
+++ src/terrain_editor.c
@@ -36,15 +36,19 @@
 struct terrain_editor_applet {
     struct applet base;
     struct place *place; /* place being edited */
-    int x, y; /* cursor location in place (used?) */
+    int x, y; /* cursor location in place (at least initially) */
     int max_cols; /* of the palette window */
     int max_rows; /* of the palette window */
     int top_index; /* index of terrain in ulc of status window */
     int max_top_index; /* where last entry is on the bottom of the palette window */
+    struct terrain *terrain; /* currently selected terrain to paint with */
+    struct terrain *quick[10]; /* quick terrain selection table */
 };
 
 static void print_terraform_help (void)
 {
+    /* fixme: no longer visible because the status window is enlarged; move to
+     * a 'help' pane */
     log_msg("");
     log_msg(" PageUp/PageDn/ = Scroll palette up/down");
     log_msg("      Home/End  = Scroll to begin/end");
@@ -63,11 +67,9 @@ static void print_terraform_help (void)
  * emit_terraform_status - print the active terrain palette entry to the
  * console during terraform mode
  */
-void emit_terraform_status (char * msg, struct terrain_palette * pp, 
-                            struct terrain * tt)
+void emit_terraform_status (char * msg, struct terrain * tt)
 {
-    log_msg("[%s] %3d: %s '%s'", msg, pp->current_terrain_index, tt->tag, 
-            tt->name);
+    log_msg("[%s]: %s '%s'", msg, tt->tag, tt->name);
 }
 
 
@@ -102,13 +104,8 @@ static void cmd_terraform_fill(struct terrain *nt, struct terrain *ot, struct pl
  */
 static void terrain_editor_change_xy(struct terrain_editor_applet *tea, int x, int y)
 {
-    struct terrain_map     * map = tea->place->terrain_map;
-    struct terrain_palette * pp  = map->palette;
-    struct terrain         * tt  = palette_current_terrain(pp);
-
-    //terrain_map_fill(map, x, y, 1, 1, tt); /* fixme: use place_set_terrain instead? */
-    if (tt != place_get_terrain(tea->place, x, y)) {
-        place_set_terrain(tea->place, x, y, tt);
+    if (tea->terrain != place_get_terrain(tea->place, x, y)) {
+        place_set_terrain(tea->place, x, y, tea->terrain);
         vmask_invalidate(tea->place, x, y, 1, 1);
         mapSetDirty();
         mapUpdate(0);
@@ -136,16 +133,10 @@ static void terrain_editor_look_at_xy(struct terrain_editor_applet *tea, int x, 
  */
 static int terrain_editor_key_handler(struct KeyHandler * kh, int key, int keymod)
 {
-    struct terrain_palette * pp;
-    struct terrain * tt;
-    struct session *session;
-
-    assert(kh);
-
     DECL_CAST(struct terrain_editor_applet, tea, kh->data);
+    struct session *session = tea->base.session;
 
-    pp = tea->place->terrain_map->palette;
-    session = tea->base.session;
+    printf("key=%x keymod=%x\n", key, keymod);
 
     if (key == '\n' || key == SDLK_SPACE || key == SDLK_RETURN || key == SDLK_LCTRL || key == SDLK_RCTRL) {
         int x = session->crosshair->getX();
@@ -176,35 +167,27 @@ static int terrain_editor_key_handler(struct KeyHandler * kh, int key, int keymo
 
     if (key == 'c') {
         /* Set the terrain beneath the cursor as the current "pen" */
-        int index = -1;
-        tt = place_get_terrain(session->crosshair->getPlace(),
-                               session->crosshair->getX(),
-                               session->crosshair->getY());
-        index = palette_get_terrain_index(pp, tt);
-        if (index >= 0) {
-            palette_set_current_terrain(pp, index);
-            emit_terraform_status("Copy", pp, tt);
-        }
+        tea->terrain = place_get_terrain(session->crosshair->getPlace(),
+                                         session->crosshair->getX(),
+                                         session->crosshair->getY());
+        emit_terraform_status("Copy", tea->terrain);
         return 0;
     }
 
     if (key == 'f') {
         /* "Fill" using the 4-neighbors algorithm */
-        tt = palette_current_terrain(pp);
-        if (tt) {
-            struct terrain *ot;
-            ot = place_get_terrain(session->crosshair->getPlace(),
-                                   session->crosshair->getX(),
-                                   session->crosshair->getY());
-            if (tt != ot) {
-                cmd_terraform_fill(tt, ot,
-                                   session->crosshair->getPlace(),
-                                   session->crosshair->getX(),
-                                   session->crosshair->getY());
-                emit_terraform_status("Flood-Fill", pp, tt);
-            }
-            mapUpdate(0);
+        struct terrain *ot;
+        ot = place_get_terrain(session->crosshair->getPlace(),
+                               session->crosshair->getX(),
+                               session->crosshair->getY());
+        if (tea->terrain != ot) {
+            cmd_terraform_fill(tea->terrain, ot,
+                               session->crosshair->getPlace(),
+                               session->crosshair->getX(),
+                               session->crosshair->getY());
+            emit_terraform_status("Flood-Fill", tea->terrain);
         }
+        mapUpdate(0);
         return 0;
     }
 
@@ -214,44 +197,28 @@ static int terrain_editor_key_handler(struct KeyHandler * kh, int key, int keymo
     }
 
     if (key == SDLK_PAGEUP) {
-        // Page Up == Cycle back through terrain in palette
-        palette_prev_terrain(pp);
-        tt = palette_current_terrain(pp);
-        emit_terraform_status("Prev", pp, tt);
         if (tea->top_index >= tea->max_cols) {
             tea->top_index -= tea->max_cols;
             statusRepaint();
         }
-        return 0;  /* Keep on keyhandling */
+        return 0;
     }
     if (key == SDLK_PAGEDOWN) {
-        // Page Down == Cycle forward through terrain in palette
-        palette_next_terrain(pp);
-        tt = palette_current_terrain(pp);
-        emit_terraform_status("Next", pp, tt);
         if (tea->top_index < tea->max_top_index) {
             tea->top_index += tea->max_cols;
             statusRepaint();
         }
-        return 0;  /* Keep on keyhandling */
+        return 0;
     }
     if (key == SDLK_HOME) {
-        // Home == Select first terrain in palette
-        palette_first_terrain(pp);
-        tt = palette_current_terrain(pp);
-        emit_terraform_status("Frst", pp, tt);
         tea->top_index = 0;
         statusRepaint();
-        return 0;  /* Keep on keyhandling */
+        return 0;
     }
     if (key == SDLK_END) {
-        // End == Select last terrain in palette
-        palette_last_terrain(pp);
-        tt = palette_current_terrain(pp);
-        emit_terraform_status("Last", pp, tt);
         tea->top_index = tea->max_top_index;
         statusRepaint();
-        return 0;  /* Keep on keyhandling */
+        return 0;
     }
 
     if (key >= '0' && key <= '9') {
@@ -260,20 +227,19 @@ static int terrain_editor_key_handler(struct KeyHandler * kh, int key, int keymo
     
         if ((keymod && KMOD_LCTRL) || (keymod && KMOD_RCTRL)) {
             // Control-NUM == set quick terrain to current:
-            int index = palette_get_current_terrain_index(pp);
-            palette_set_quick_terrain(pp, qt, index);
-            tt = palette_current_terrain(pp);
-            log_msg("[Set Quick %d] %3d: %s '%s'", qt, 
-                    pp->current_terrain_index, tt->tag, tt->name);
-            return 0; /* Keep on keyhandling */
+            tea->quick[qt] = tea->terrain;
+            log_msg("[Set Quick %d]: %s '%s'", qt, tea->terrain->tag, tea->terrain->name);
+            return 0;
+        } else {
+            // Plain NUM == set current terrain from quick terrain:
+            if (!tea->quick[qt]) {
+                log_msg("[Quick %d]: empty!");
+            } else {
+                tea->terrain = tea->quick[qt];
+                log_msg("[Quick %d]: %s '%s'", qt, tea->terrain->tag, tea->terrain->name);
+            }
         }
-        // Plain NUM == set current terrain from quick terrain:
-        int index = palette_get_quick_terrain_index(pp, qt);
-        palette_set_current_terrain(pp, index);
-        tt = palette_current_terrain(pp);
-        log_msg("[Quick %d] %3d: %s '%s'", qt, 
-                pp->current_terrain_index, tt->tag, tt->name);
-        return 0;  /* Keep on keyhandling */
+        return 0;
     }
 
     if (key == SDLK_ESCAPE) {
@@ -306,10 +272,43 @@ static bool terrain_editor_map_click(struct terrain_editor_applet *tea, int mx, 
         terrain_editor_look_at_xy(tea, mx, my);
     }
 #endif
-    printf("(%d %d)\n", mx, my);
     terrain_editor_change_xy(tea, mx, my);
 
     return false;
+}
+
+/**
+ * terrain_editor_next - return the next terrain in the session terrain list or
+ * NULL if this is the last.
+ */
+static terrain *terrain_editor_next(struct terrain_editor_applet *tea, struct terrain *ter)
+{
+    struct list *head = &tea->base.session->terrains;
+    struct list *elem = ter->session_list.next;
+    if (elem == head) {
+        return NULL;
+    }
+    return list_entry(elem, struct terrain, session_list);
+}
+
+/**
+ * terrain_editor_lookup - find the terrain at the given index in the session
+ * terrain list and return it, or NULL if the index is beyond the end of the
+ * list.
+ */
+static terrain *terrain_editor_lookup(struct terrain_editor_applet *tea, int index)
+{
+    struct list *head = &tea->base.session->terrains;
+    struct list *elem = head->next;
+    while (index && (elem != head)) {
+        index--;
+        elem = elem->next;
+    }
+    if (index) {
+        return NULL;
+    }
+
+    return list_entry(elem, struct terrain, session_list);
 }
 
 /**
@@ -318,8 +317,6 @@ static bool terrain_editor_map_click(struct terrain_editor_applet *tea, int mx, 
  */
 static bool terrain_editor_palette_click(struct terrain_editor_applet *tea, int sx, int sy)
 {
-    struct terrain_palette *palette = tea->place->terrain_map->palette;
-
     /* Convert screen coordinates to row and column */
     int row = (sy - tea->base.dims.y) / TILE_H;
     int col = (sx - tea->base.dims.x) / TILE_W;
@@ -327,9 +324,11 @@ static bool terrain_editor_palette_click(struct terrain_editor_applet *tea, int 
     /* Convert row and column to palette index */
     int index = tea->top_index + col + (row * tea->max_cols);
     
-    /* Check */
-    if (index < palette->num_entries) {
-        palette_set_current_terrain(palette, index);
+    /* Lookup */
+    struct terrain *newter = terrain_editor_lookup(tea, index);
+    if (newter) {
+        tea->terrain = newter;
+        emit_terraform_status("Set ", tea->terrain);
     }
 
     return false;
@@ -391,6 +390,8 @@ static void terrain_editor_applet_ops_run(struct applet *applet, SDL_Rect *dims,
     DECL_CAST(struct terrain_editor_applet, tea, applet);
     tea->max_cols = dims->w / TILE_W;
     tea->max_rows = dims->h / TILE_H;
+    assert(! list_empty(&session->terrains));
+    tea->terrain = list_entry(session->terrains.next, struct terrain, session_list);
     tea->max_top_index = tea->place->terrain_map->palette->num_entries - (tea->max_cols * tea->max_rows);
     if (tea->max_top_index < 0) {
         tea->max_top_index = 0;
@@ -406,20 +407,16 @@ static void terrain_editor_applet_ops_run(struct applet *applet, SDL_Rect *dims,
     cmdwin_spush("<target> (ESC to exit)");
 
     /* Initialize the console */
-    struct terrain_palette *palette = tea->place->terrain_map->palette;
-    struct terrain *terrain = palette_current_terrain(palette);
-    
     log_begin_group();
     log_msg("---Terraform---");
     log_msg("Place %s",     tea->place->tag  );
     log_msg("      \"%s\"", tea->place->name );
     log_msg("Map   %s",     tea->place->terrain_map->tag    );
-    log_msg("Palette %s",   palette->tag);
     log_msg("");
     
     print_terraform_help();
     log_msg("Press '?' for Terraform command help.");
-    emit_terraform_status("Trrn", palette, terrain);
+    emit_terraform_status("Set ", tea->terrain);
     terrain_editor_look_at_xy(tea, tea->x, tea->y);
 
     /* Position the cursor */
@@ -475,17 +472,15 @@ static void terrain_editor_applet_ops_paint(struct applet *applet)
     DECL_CAST(struct terrain_editor_applet, tea, applet);
 
     int y = applet->dims.y;
-    struct terrain_palette_entry *tpe = palette_entry(tea->place->terrain_map->palette, tea->top_index);
-    if (tpe) {
-        for (int row = 0; tpe && (row < tea->max_rows); row++) {
-            int x = applet->dims.x;
-            for (int col = 0; tpe && (col < tea->max_cols); col++) {
-                sprite_paint(tpe->terrain->sprite, 0, x, y);
-                tpe = palette_entry_next(tea->place->terrain_map->palette, tpe);
-                x += TILE_W;
-            }
-            y += TILE_H;
+    struct terrain *ter = terrain_editor_lookup(tea, tea->top_index);
+    for (int row = 0; ter && (row < tea->max_rows); row++) {
+        int x = applet->dims.x;
+        for (int col = 0; ter && (col < tea->max_cols); col++) {
+            sprite_paint(ter->sprite, 0, x, y);
+            ter = terrain_editor_next(tea, ter);
+            x += TILE_W;
         }
+        y += TILE_H;
     }
     screenUpdate(&applet->dims);
     status_repaint_title();    
