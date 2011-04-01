5d7d52b2d97d81867ed2a3fc4a0641d0a6a1ed6f
Gordon McNutt <gmcnutt@users.sourceforge.net>
2011-04-01 04:48:33 +0000

    Removed obsolete player gob ctor.

--- worlds/haxima-1.002/player.scm
+++ worlds/haxima-1.002/player.scm
@@ -1,10 +1,4 @@
 ;; This defines the gob for the player party in haxima.
 ;; This sort of thing would probably be best stored in quest flags.
-(define (player-gob) (gob (kern-get-player)))
-(define (mk-player-gob) (list 
-                         #f ;; found warritix
-                         #f ;; trial of stewardess done
-                         ))
 (define (player-found-warritrix?) (not (null? (quest-data-getvalue 'questentry-warritrix 'found))))
 (define (player-stewardess-trial-done?) (not (null? (quest-data-getvalue 'questentry-warritrix 'avenged))))
-
