b7b2a4d6f8687f92e427b5860216a2cb3a8b8235
Gordon McNutt <gmcnutt@cableone.net>
2012-10-26 08:15:42 -0600

    Added haxima-1.003, removed some old, old docs files

---
+++ worlds/haxima-1.003/kern-init.scm
@@ -0,0 +1,40 @@
+(kern-cfg-set 
+
+ ;; This is the image file for the UI border. The pieces need to be arranged in
+ ;; a specific order in this image.
+ "frame-image-filename"  "images/system/frame.png"
+
+ ;; These are the letters used by the console, etc, in the UI. The character
+ ;; sprites need to be arranged in a specific order in this image.
+ "ascii-image-filename"  "images/system/charset.png"
+
+ ;; This is the cursor prompt used by the command window in the UI. It should
+ ;; have four animation frames.
+ "cursor-image-filename" "images/system/cursor.png"
+
+ ;; This is the script file run when the user selects the "Start New Game"
+ ;; option from the main menu.
+ "new-game-filename"     "start-new-game.scm"
+
+ ;; This is the script file run when the user selects the "Journey Onward"
+ ;; option from the main menu. It lists the current save files.
+ "save-game-filename"     "saved-games.scm"
+
+ ;; This is the script file run when the user selects the "Tutorial"
+ ;; option from the main menu.
+ "tutorial-filename"     "tutorial.scm"
+
+ ;; This is the script file which runs the demo scene on startup.
+ "demo-filename" "demo.scm"
+
+ ;; These are the filenames of the splash image shown on startup for the
+ ;; various supported screen sizes. The format of the key must be
+ ;; <width>x<height>-splash-image-filename.
+ "1280x960-splash-image-filename" "images/system/splash.png"
+ "640x480-splash-image-filename" "images/system/640x480_splash.png"
+ "800x480-splash-image-filename" "images/system/640x480_splash.png"
+
+;; This is the image for the sprite pieces of the progress bar.
+"progress-bar-image-filename" "images/system/progress_bar_image.png"
+
+ )
